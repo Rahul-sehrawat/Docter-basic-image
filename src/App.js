79534f8906fa92a834eec365b63a54c2018c3bcb
Rahul sehrawat <rahulsehrawat1205@gmail.com>
2024-08-13 18:55:44 +0530

    docker basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hii, this is a docker demo project</h1>
+      <br />
+      <h2>this is version:2 of this docker demo project</h2>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
